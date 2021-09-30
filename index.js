58a709e25d8e3be687ef0c4698d1156336de793e
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2021-09-30 14:53:13 -0400

    commit tree 494d2ab1542df48cdbdc77a73849703de1200166

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(username, userEmail) {
+    const url = 'http://localhost:3000/users'
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: username,
+            email: userEmail
+        })
+    }
+    return fetch(url, options)
+        .then(resp => resp.json())
+        .then(results => {
+            const newP = document.createElement('p');
+            newP.textContent = results.id;
+            document.querySelector('body').append(newP);
+        }).catch(error => {
+            console.error(error);
+            console.error('Attempted URL:', url);
+            console.error('Attempted Options:', options);
+            const newP = document.createElement('p');
+            newP.textContent = error;
+            document.querySelector('body').append(newP);
+        });
+}
